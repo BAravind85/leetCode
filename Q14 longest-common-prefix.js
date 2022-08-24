ac8457864de3fc0bcbc105b63571c219cde955b0
BAravind85 <102968059+BAravind85@users.noreply.github.com>
2022-08-24 14:14:18 +0530

    Q14 longest common perfiex

---
+++ Q14 longest-common-prefix.js
@@ -0,0 +1,24 @@
+let arr=["flower","flow","flight"]
+function longestCommon(arr) {
+    if(arr.length ===0){ return ""};
+    
+let a=""
+
+for (let i=0; i<arr.length; i++){
+    let char=arr[0][i]
+    for(let j=0;j<arr.length; j++){
+        if(arr[j][i]==char){  
+            continue;    
+    }else{
+        return a;
+    }
+        }
+        a+=char;
+    }
+    return a;
+}
+    console.log(longestCommon(arr))
+
+
+
+  
---
+++ maxpairsum.js
@@ -0,0 +1,22 @@
+let num=[1,2,3,89,4,10];
+function maxPair(num){
+let num2=num.sort(function(a,b){return a-b})
+let arr=num2,highest=0;
+
+ 
+for(let i=0;i<arr.length;i++){
+   let a=0;
+  for(let j=i+1;j<arr.length;j++){
+    if(arr[i]>0){
+     a=arr[i]+arr[j]
+    }    
+  }
+  if(a>highest){
+    highest=a
+    
+  }
+
+}
+      return highest
+}
+console.log(maxPair(num))
